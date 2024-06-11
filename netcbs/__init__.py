97038f9a10d3afd760fd797378c09eb1733c9308
Javier Garcia-Bernardo <j.garciabernardo@uu.nl>
2024-06-11 23:24:37 +0200

    remove private functions

--- netcbs/__init__.py
+++ netcbs/__init__.py
@@ -1,3 +1,6 @@
-from .netcbs import *
+"""Import the main functions of the package."""
 
-__version__ = '0.1'
+__all__ = ['codebook', 'context2path', 'context2types', 'format_path', 'transform', 'create_synthetic_data', "validate_query"]
+
+from .netcbs import codebook, context2path, context2types, format_path, transform, validate_query
+from .create_synthetic_data import create_synthetic_data
